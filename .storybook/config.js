4160fcdef6ffac6ce0e658375bc98edde0ef536d
Andrzej Tomala <andrzej.tomala@creativestyle.pl>
2018-04-12 14:24:47 +0200

    add missing files

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,20 @@
-import { configure } from '@storybook/react';
+import React from 'react'
+import { configure, storiesOf, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { ThemeProvider } from 'styled-components';
+import { Theme } from '../src/theme/basic';
 
-const req = require.context('../src', true, /\.story\.js$/)
+const req = require.context('../src', true, /story\.js$/)
 
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
+
+const ThemeDecorator = (storyFn) => (
+  <ThemeProvider theme={Theme}>
+    { storyFn() }
+  </ThemeProvider>
+);
+addDecorator(ThemeDecorator);
 
 configure(loadStories, module);
